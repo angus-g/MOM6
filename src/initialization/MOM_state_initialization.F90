69f10ce4379e4610ca26c845a9b55591f2899e78
Angus Gibson <angus@agibson.me>
2023-10-31 08:30:48 +1100

    Make calc_sfc_displacement public
    We want to do this outside the main state initialisation function

--- src/initialization/MOM_state_initialization.F90
+++ src/initialization/MOM_state_initialization.F90
@@ -103,6 +103,8 @@ implicit none ; private
 
 public MOM_initialize_state
 
+public :: calc_sfc_displacement, depress_surface
+
 ! A note on unit descriptions in comments: MOM6 uses units that can be rescaled for dimensional
 ! consistency testing. These are noted in comments with units like Z, H, L, and T, along with
 ! their mks counterparts with notation like "a velocity [Z T-1 ~> m s-1]".  If the units
