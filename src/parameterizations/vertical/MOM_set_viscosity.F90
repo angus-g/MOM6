e17fe42a595f7459133c1a1f28f4647817042570
Angus Gibson <angus@agibson.me>
2024-08-27 11:11:44 +1000

    Enforce positive cdrag

--- src/parameterizations/vertical/MOM_set_viscosity.F90
+++ src/parameterizations/vertical/MOM_set_viscosity.F90
@@ -2377,6 +2377,7 @@ subroutine set_visc_init(Time, G, GV, US, param_file, diag, visc, CS, restart_CS
       cdrag_file = trim(CS%inputdir)//trim(cdrag_file)
       call log_param(param_file, mdl, "INPUTDIR/CDRAG_FILE", cdrag_file)
       call MOM_read_data(cdrag_file, cdrag_var, CS%cdrag, G%Domain)
+      CS%cdrag = max(CS%cdrag, 0.0)
     else
       CS%cdrag(:,:) = scalar_cdrag
     endif
