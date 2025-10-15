c709681188733e160b032e9154c658fbe53d5616
jbisits <jbisits@gmail.com>
2025-10-15 13:51:33 +1100

    Reorder the loop in thickness weighted variance

--- src/diagnostics/MOM_numerical_mixing.F90
+++ src/diagnostics/MOM_numerical_mixing.F90
@@ -63,13 +63,15 @@ subroutine thickness_weighted_variance_change(Tr, Tr_adv_scale, h, h_tendency, d
 
   is = G%isc ; ie = G%iec ; js = G%jsc ; je = G%jec
 
-  do i = is, ie ; do j = js, je ; do k = 1, nz
-    h1 = h(i, j, k)
-    hadv = h1 + dt * h_tendency(i, j, k)
-    C1 = Tr%t(i, j, k)
-    Cadv = h1 * C1 + dt * Tr%advection_xy(i, j, k) / Tr_adv_scale
-    nm(i, j, k) = (Cadv**2 / hadv - h1 * C1**2) / dt
-  enddo ; enddo ; enddo
+  do k = 1, nz
+    do j = js, je ; do i = is, ie
+      h1 = h(i, j, k)
+      hadv = h1 + dt * h_tendency(i, j, k)
+      C1 = Tr%t(i, j, k)
+      Cadv = h1 * C1 + dt * Tr%advection_xy(i, j, k) / Tr_adv_scale
+      nm(i, j, k) = (Cadv**2 / hadv - h1 * C1**2) / dt
+    enddo ; enddo
+  enddo
 
 end subroutine thickness_weighted_variance_change
 
