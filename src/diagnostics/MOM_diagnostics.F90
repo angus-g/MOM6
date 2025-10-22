6436bf565995c179337c4cbf36ed5f0ce80db4aa
jbisits <jbisits@gmail.com>
2025-10-22 13:45:28 +1100

    Return nm array to correct size

--- src/diagnostics/MOM_diagnostics.F90
+++ src/diagnostics/MOM_diagnostics.F90
@@ -1649,7 +1649,7 @@ subroutine post_transport_diagnostics(G, GV, US, uhtr, vhtr, h, IDs, diag_pre_dy
   real, dimension(SZI_(G),SZJB_(G),SZK_(GV)) :: vmo ! Diagnostics of layer mass transport [R Z L2 T-1 ~> kg s-1]
   real, dimension(SZI_(G),SZJ_(G),SZK_(GV))   :: h_tend ! Change in layer thickness due to dynamics
                           ! [H T-1 ~> m s-1 or kg m-2 s-1].
-  real, dimension(SZIB_(G),SZJ_(G),SZK_(GV))   :: nm ! Numerical mixing of a tracer
+  real, dimension(SZI_(G),SZJ_(G),SZK_(GV))   :: nm ! Numerical mixing of a tracer
   real :: Idt             ! The inverse of the time interval [T-1 ~> s-1]
   real :: H_to_RZ_dt      ! A conversion factor from accumulated transports to fluxes
                           ! [R Z H-1 T-1 ~> kg m-3 s-1 or s-1].
