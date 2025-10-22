311f1e8ba987eb4765d8bead9a7c0fe56cffd8be
jbisits <jbisits@gmail.com>
2025-10-22 16:40:58 +1100

    Revert to correct units for nm in variable and diagnostic

--- src/tracer/MOM_tracer_registry.F90
+++ src/tracer/MOM_tracer_registry.F90
@@ -380,10 +380,8 @@ subroutine register_tracer_diagnostics(Reg, h, Time, diag, G, GV, US, use_ALE, u
           "flux from the horizontal boundary diffusion scheme", trim(flux_units), &
           v_extensive=.true., &
           x_cell_method='sum', conversion=(US%L_to_m**2)*Tr%flux_scale*US%s_to_T)
-      ! Tr%id_numerical_mixing = register_diag_field("ocean_model", trim(shortnm)//"_numerical_mixing", &
-      !             diag%axesTL, Time, "Spurious mixing of "//trim(shortnm)//" due to advection", trim(Tr%units)//"^2ms-1")
       Tr%id_numerical_mixing = register_diag_field("ocean_model", trim(shortnm)//"_numerical_mixing", &
-                  diag%axesCuL, Time, "Spurious mixing of "//trim(shortnm)//" due to advection", trim(Tr%units)//"^2ms-1")
+                  diag%axesTL, Time, "Spurious mixing of "//trim(shortnm)//" due to advection", trim(Tr%units)//"^2ms-1")
     else
       Tr%id_adx = register_diag_field("ocean_model", trim(shortnm)//"_adx", &
           diag%axesCuL, Time, "Advective (by residual mean) Zonal Flux of "//trim(flux_longname), &
@@ -409,10 +407,8 @@ subroutine register_tracer_diagnostics(Reg, h, Time, diag, G, GV, US, use_ALE, u
           "Horizontal Boundary Diffusive Meridional Flux of "//trim(flux_longname), &
           flux_units, v_extensive=.true., conversion=(US%L_to_m**2)*Tr%flux_scale*US%s_to_T, &
           x_cell_method='sum')
-      ! Tr%id_numerical_mixing = register_diag_field("ocean_model", trim(shortnm)//"_numerical_mixing", &
-      !             diag%axesTL, Time, "Spurious mixing of "//trim(shortnm)//" due to advection", trim(Tr%units)//"^2ms-1")
       Tr%id_numerical_mixing = register_diag_field("ocean_model", trim(shortnm)//"_numerical_mixing", &
-                  diag%axesCuL, Time, "Spurious mixing of "//trim(shortnm)//" due to advection", trim(Tr%units)//"^2ms-1")
+                  diag%axesTL, Time, "Spurious mixing of "//trim(shortnm)//" due to advection", trim(Tr%units)//"^2ms-1")
     endif
     Tr%id_zint = register_diag_field("ocean_model", trim(shortnm)//"_zint", &
         diag%axesT1, Time, &
