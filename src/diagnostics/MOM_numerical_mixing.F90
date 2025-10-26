5e7ff994f2bb5bb3006d02ced2c755eb687c07d3
jbisits <jbisits@gmail.com>
2025-10-27 10:42:59 +1100

    Try with previous h_state as h1

--- src/diagnostics/MOM_numerical_mixing.F90
+++ src/diagnostics/MOM_numerical_mixing.F90
@@ -64,20 +64,16 @@ subroutine thickness_weighted_variance_change(Tr, Tr_adv_scale, h, diag_pre_dyn,
   integer :: i, j, k             !< Counters
   real :: h1, C1, hadv, Cadv     !< Temporary variables for thickness and tracer at current timestep
                                  !< and the changes in thickness and tracer due to advection.
-  ! real, dimension(SZI_(G),SZJ_(G),SZK_(GV)) :: h_tend  !< Thickness tendenccy
 
   is = G%isc ; ie = G%iec ; js = G%jsc ; je = G%jec ; nz = GV%ke
 
-  ! h_tend(:,:,:) = 0.
-  ! do k=1,nz ; do j=js,je ; do i=is,ie
-  !   h_tend(i,j,k) = (h(i,j,k) - diag_pre_dyn%h_state(i,j,k))*Idt
-  ! enddo ; enddo ; enddo
-
   do k = 1, nz
     do j = js, je ; do i = is, ie
-      h1 = h(i, j, k)
+      ! h1 = h(i, j, k)
       ! hadv = h1 + dt * h_tend(i, j, k)
-      hadv = 2*h1 - diag_pre_dyn%h_state(i,j,k)
+      ! hadv = 2*h1 - diag_pre_dyn%h_state(i,j,k) ! hadv = h1 + dt * h_tend(i, j, k) =
+      h1 = diag_pre_dyn%h_state(i, j, k)
+      hadv = h(i, j, k)
       C1 = Tr%t(i, j, k)
       Cadv = h1 * C1 +  dt * (Tr%advection_xy(i, j, k)  / Tr_adv_scale)
       nm(i, j, k) = ( (Cadv**2 / hadv) - (h1 * C1**2) ) * Idt
