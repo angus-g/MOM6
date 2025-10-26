39647827b1093d395706df96fe2f09c5eb69046f
jbisits <jbisits@gmail.com>
2025-10-27 09:41:15 +1100

    Move tendency calculation into numerical mixing, should be able to call without needing to save other variables

--- src/diagnostics/MOM_diagnostics.F90
+++ src/diagnostics/MOM_diagnostics.F90
@@ -1717,7 +1717,7 @@ subroutine post_transport_diagnostics(G, GV, US, uhtr, vhtr, h, IDs, diag_pre_dy
       x_upwind(:, :, :) = 0.
       y_upwind(:, :, :) = 0.
       nm(:,:,:) = 0.
-      call numerical_mixing(G, GV, Tr, h, h_tend, dt_trans, Idt, uhtr, vhtr, scale_constant, x_upwind, y_upwind, nm)
+      call numerical_mixing(G, GV, Tr, h, diag_pre_dyn, dt_trans, Idt, uhtr, vhtr, scale_constant, x_upwind, y_upwind, nm)
       ! The call to numerical mixing requires other variables that may not be registered. Don't know what best practice is
       ! but one option is to add another to condition to the loops so it is computed, then another condition to post?
       call post_data(Tr%id_numerical_mixing, nm, diag)
--- src/diagnostics/MOM_numerical_mixing.F90
+++ src/diagnostics/MOM_numerical_mixing.F90
@@ -1,7 +1,7 @@
 !> Functions and routines involved in calculating numerical mixing of tracers due to advection
 module MOM_numerical_mixing
 
-use MOM_diag_mediator, only : diag_ctrl
+use MOM_diag_mediator, only : diag_ctrl, diag_grid_storage
 use MOM_grid,          only : ocean_grid_type
 use MOM_tracer_types,  only : tracer_type
 use MOM_verticalGrid,  only : verticalGrid_type
@@ -15,14 +15,14 @@ public numerical_mixing
 contains
 
 !< Calculate the suprious ``numerical'' mixing of tracer C due to advection.
-subroutine numerical_mixing(G, GV, Tr, h, h_tendency, dt, Idt, uhtr, vhtr, scale_constant, x_upwind, y_upwind, nm)
+  subroutine numerical_mixing(G, GV, Tr, h, diag_pre_dyn, dt, Idt, uhtr, vhtr, scale_constant, x_upwind, y_upwind, nm)
 
   implicit none
   type(ocean_grid_type),   intent(in) :: G                    !< Ocean grid structure
   type(verticalGrid_type), intent(in) :: GV                   !< Ocean vertical grid structure
   type(tracer_type),       intent(in) :: Tr                   !< Tracer
   real,                    intent(in) :: h(:, :, :)           !< Thickness
-  real,                    intent(in) :: h_tendency(:, :, :)  !< Thickness tendency
+  type(diag_grid_storage), intent(in) :: diag_pre_dyn         !< Stored grids from before dynamics
   real,                    intent(in) :: dt                   !< Model timestep
   real,                    intent(in) :: Idt                  !< Inverse model timestep
   real,                    intent(in) :: uhtr(:, :, :)        !< Accumulated zonal transport
@@ -39,20 +39,20 @@ subroutine numerical_mixing(G, GV, Tr, h, h_tendency, dt, Idt, uhtr, vhtr, scale
   Tr_adv_scale = scale_constant * GV%Rho0
   mass_transport_scale =(Idt * GV%H_to_RZ) / GV%Rho0
 
-  call thickness_weighted_variance_change(Tr, Tr_adv_scale, h, h_tendency, dt, Idt, G, GV, nm)
+  call thickness_weighted_variance_change(Tr, Tr_adv_scale, h, diag_pre_dyn, dt, Idt, G, GV, nm)
   call zonal_upwind_fluxes(Tr, Tr_adv_scale, uhtr, mass_transport_scale, G, GV, x_upwind, nm)
   call meridional_upwind_fluxes(Tr, Tr_adv_scale, vhtr, mass_transport_scale, G, GV, y_upwind, nm)
 
 end subroutine numerical_mixing
 
 !< Subroutine to calculate the thickness weighted variance change over a timestep
-subroutine thickness_weighted_variance_change(Tr, Tr_adv_scale, h, h_tendency, dt, Idt, G, GV, nm)
+subroutine thickness_weighted_variance_change(Tr, Tr_adv_scale, h, diag_pre_dyn, dt, Idt, G, GV, nm)
 
   implicit none
   type(tracer_type),       intent(in) :: Tr                   !< Tracer
   real,                    intent(in) :: Tr_adv_scale         !< Scaling for tracer advection
   real,                    intent(in) :: h(:, :, :)           !< Thickness
-  real,                    intent(in) :: h_tendency(:, :, :)  !< Thickness tendency
+  type(diag_grid_storage), intent(in) :: diag_pre_dyn         !< Stored grids from before dynamics
   real,                    intent(in) :: dt                   !< Model timestep
   real,                    intent(in) :: Idt                  !< Inverse model timestep
   type(ocean_grid_type),   intent(in) :: G                    !< Ocean grid structure
@@ -64,13 +64,19 @@ subroutine thickness_weighted_variance_change(Tr, Tr_adv_scale, h, h_tendency, d
   integer :: i, j, k             !< Counters
   real :: h1, C1, hadv, Cadv     !< Temporary variables for thickness and tracer at current timestep
                                  !< and the changes in thickness and tracer due to advection.
+  real, dimension(SZI_(G),SZJ_(G),SZK_(GV)) :: h_tend  !< Thickness tendenccy
 
   is = G%isc ; ie = G%iec ; js = G%jsc ; je = G%jec ; nz = GV%ke
 
+  h_tend(:,:,:) = 0.
+  do k=1,nz ; do j=js,je ; do i=is,ie
+    h_tend(i,j,k) = (h(i,j,k) - diag_pre_dyn%h_state(i,j,k))*Idt
+  enddo ; enddo ; enddo
+  
   do k = 1, nz
     do j = js, je ; do i = is, ie
       h1 = h(i, j, k)
-      hadv = h1 + dt * h_tendency(i, j, k)
+      hadv = h1 + dt * h_tend(i, j, k)
       C1 = Tr%t(i, j, k)
       Cadv = h1 * C1 +  dt * (Tr%advection_xy(i, j, k)  / Tr_adv_scale)
       nm(i, j, k) = ( (Cadv**2 / hadv) - (h1 * C1**2) ) * Idt
