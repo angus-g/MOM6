89410e61252a53b7ed7ea344b4950296c56c6d94
jbisits <jbisits@gmail.com>
2025-11-11 20:36:54 +1100

    Change worked?

--- config_src/drivers/nuopc_cap/mom_cap.F90
+++ config_src/drivers/nuopc_cap/mom_cap.F90
@@ -154,7 +154,6 @@ logical              :: profile_memory = .true.
 logical              :: grid_attach_area = .false.
 logical              :: use_coldstart = .true.
 logical              :: use_mommesh = .true.
-logical              :: set_missing_stks_to_zero = .false.
 logical              :: restart_eor = .false.
 character(len=128)   :: scalar_field_name = ''
 integer              :: scalar_field_count = 0
@@ -381,14 +380,6 @@ subroutine InitializeP0(gcomp, importState, exportState, clock, rc)
   if (isPresent .and. isSet) use_coldstart=(trim(value)=="true")
   write(logmsg,*) use_coldstart
   call ESMF_LogWrite('MOM_cap:use_coldstart = '//trim(logmsg), ESMF_LOGMSG_INFO)
-
-  set_missing_stks_to_zero = .false.
-  call NUOPC_CompAttributeGet(gcomp, name="set_missing_stks_to_zero", value=value, &
-       isPresent=isPresent, isSet=isSet, rc=rc)
-  if (ChkErr(rc,__LINE__,u_FILE_u)) return
-  if (isPresent .and. isSet) set_missing_stks_to_zero=(trim(value)=="true")
-  write(logmsg,*) set_missing_stks_to_zero
-  call ESMF_LogWrite('MOM_cap:set_missing_stks_to_zero = '//trim(logmsg), ESMF_LOGMSG_INFO)
 
   use_mommesh = .true.
   call NUOPC_CompAttributeGet(gcomp, name="use_mommesh", value=value, &
