86864171a5fe794cd7a0f65edc7017929da60e36
jbisits <jbisits@gmail.com>
2025-11-13 10:45:24 +1100

    Missed then

--- src/diagnostics/MOM_diagnostics.F90
+++ src/diagnostics/MOM_diagnostics.F90
@@ -1729,7 +1729,7 @@ subroutine post_transport_diagnostics(G, GV, US, uhtr, vhtr, h, IDs, diag_pre_dy
       call numerical_mixing(G, GV, Tr, h, diag_pre_dyn, dt_trans, Idt, uhtr, vhtr, &
                             scale_constant, x_upwind, y_upwind, nm)
       call post_data(Tr%id_numerical_mixing, nm, diag, alt_h=diag_pre_dyn%h_state)
-      if (Tr%id_variance_adv > 0)
+      if (Tr%id_variance_adv > 0) then
         va(:,:,:) = 0.
         call variance_advection(G, GV, Tr, h, diag_pre_dyn, dt, Idt, scale_constant, va)
         call post_data(Tr%id_variance_adv, va, diag, alt_h=diag_pre_dyn%h_state)
