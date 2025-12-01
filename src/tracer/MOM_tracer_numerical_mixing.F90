b51294c481d070320927879d755e8dc0bd7b453b
jbisits <jbisits@gmail.com>
2025-12-01 13:19:56 +1100

    Missed a bracket

--- src/tracer/MOM_tracer_numerical_mixing.F90
+++ src/tracer/MOM_tracer_numerical_mixing.F90
@@ -161,7 +161,7 @@ subroutine zonal_upwind_fluxes(Tr, ITR_adv_scale, uhtr, mass_transport_scale, G,
     ! east = 2 * (Tr%ad_x(I,j,k)   * ITR_adv_scale) * x_upwind(I,j,k)   - u_trans(I,j,k)   * x_upwind(I,j,k)**2
     ! west = 2 * (Tr%ad_x(I-1,j,k) * ITR_adv_scale) * x_upwind(I-1,j,k) - u_trans(I-1,j,k) * x_upwind(I-1,j,k)**2
     east = 2 * (Tr%ad_x(I,j,k)   * x_upwind(I,j,k))  - uhtr(I,j,k)   * x_upwind(I,j,k)**2
-    west = 2 * (Tr%ad_x(I-1,j,k) * x_upwind(I-1,j,k) - uhtr(I-1,j,k) * x_upwind(I-1,j,k)**2
+    west = 2 * (Tr%ad_x(I-1,j,k) * x_upwind(I-1,j,k)) - uhtr(I-1,j,k) * x_upwind(I-1,j,k)**2
     res(i,j,k) = res(i,j,k) + ((east - west) * G%IareaT(i,j))
   enddo ; enddo; enddo
 
