d9d39a2e72cce4052c874b4691db30586ff83e4e
Josef Bisits <jbisits@gmail.com>
2025-12-05 13:41:36 +1100

    Index the online calculation correctly for ANU-TUB

--- src/tracer/MOM_tracer_numerical_mixing.F90
+++ src/tracer/MOM_tracer_numerical_mixing.F90
@@ -129,8 +129,8 @@ subroutine thickness_weighted_zonal_variance_flux(Tr, uhtr, G, GV, Idt, x_upwind
   call zonal_upwind_values(Tr, G, nz, uhtr, x_upwind)
 
   do k=1,nz ;  do j=js,je ; do i=is,ie
-    east = (2 * Tr%ad_x(I,j,k)   * x_upwind(I,j,k))   - (Idt * uhtr(I,j,k)   * x_upwind(I,j,k)**2)
-    west = (2 * Tr%ad_x(I-1,j,k) * x_upwind(I-1,j,k)) - (Idt * uhtr(I-1,j,k) * x_upwind(I-1,j,k)**2)
+    east = (2 * Tr%ad_x(I,j,k)   * x_upwind(I,j,k))   - (Idt * uhtr(I+1,j,k)   * x_upwind(I,j,k)**2)
+    west = (2 * Tr%ad_x(I-1,j,k) * x_upwind(I-1,j,k)) - (Idt * uhtr(I,j,k) * x_upwind(I-1,j,k)**2)
     res(i,j,k) = res(i,j,k) + ((east - west) * G%IareaT(i,j))
   enddo ; enddo; enddo
 
@@ -185,8 +185,8 @@ subroutine thickness_weighted_meridional_variance_flux(Tr, vhtr, G, GV, Idt, y_u
   call meridional_upwind_values(Tr, G, nz, vhtr, y_upwind)
 
   do k=1,nz ; do j=js,je ; do i=is,ie
-    north = (2 * Tr%ad_y(i,J,k)   * y_upwind(i,J,k))   - (Idt * vhtr(i,J,k)   * y_upwind(i,J,k)**2)
-    south = (2 * Tr%ad_y(i,J-1,k) * y_upwind(i,J-1,k)) - (Idt * vhtr(i,J-1,k) * y_upwind(i,J-1,k)**2)
+    north = (2 * Tr%ad_y(i,J,k)   * y_upwind(i,J,k))   - (Idt * vhtr(i,J+1,k)   * y_upwind(i,J,k)**2)
+    south = (2 * Tr%ad_y(i,J-1,k) * y_upwind(i,J-1,k)) - (Idt * vhtr(i,J,k) * y_upwind(i,J-1,k)**2)
     res(i,j,k) = res(i,j,k) + ((north - south) * G%IareaT(i,j))
   enddo ; enddo ; enddo
 
