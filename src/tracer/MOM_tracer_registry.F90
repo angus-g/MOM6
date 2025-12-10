ebb3ad75059948764ac6d616a60dfcdafbcd3271
jbisits <jbisits@gmail.com>
2025-12-10 22:07:35 +1100

    Only update for non-symmetric

--- src/tracer/MOM_tracer_registry.F90
+++ src/tracer/MOM_tracer_registry.F90
@@ -12,7 +12,8 @@ use MOM_debugging,     only : hchksum
 use MOM_diag_mediator, only : diag_ctrl, register_diag_field, post_data, safe_alloc_ptr
 use MOM_diag_mediator, only : diag_grid_storage
 use MOM_diag_mediator, only : diag_copy_storage_to_diag, diag_save_grids, diag_restore_grids
-use MOM_domains,           only : create_group_pass, do_group_pass, group_pass_type
+use MOM_domains,       only : create_group_pass, do_group_pass, group_pass_type
+! use MOM_domains,       only : To_North, To_East
 use MOM_error_handler, only : MOM_error, FATAL, WARNING, MOM_mesg, is_root_pe
 use MOM_file_parser,   only : get_param, log_version, param_file_type
 use MOM_hor_index,     only : hor_index_type
@@ -866,9 +867,11 @@ subroutine post_tracer_transport_diagnostics(G, GV, Reg, h_diag, diag_pre_dyn, d
       x_upwind(:,:,:) = 0.
       y_upwind(:,:,:) = 0.
       nm(:,:,:) = 0.
-      call create_group_pass(pass_adx_ady, Tr%ad_x, G%Domain)
-      call create_group_pass(pass_adx_ady, Tr%ad_y, G%Domain)
-      call do_group_pass(pass_adx_ady, G%Domain)
+      if (.not.G%symmetric) then
+        call create_group_pass(pass_adx_ady, Tr%ad_x, G%Domain)
+        call create_group_pass(pass_adx_ady, Tr%ad_y, G%Domain)
+        call do_group_pass(pass_adx_ady, G%Domain)
+      endif
       call numerical_mixing(G, GV, Tr, h, diag_pre_dyn, dt_trans, Idt, uhtr, vhtr, &
                             x_upwind, y_upwind, nm)
       call post_data(Tr%id_numerical_mixing, nm, diag, alt_h=diag_pre_dyn%h_state)
