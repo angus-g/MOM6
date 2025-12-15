62da295ee16ff0a1c346da4872fddf665d50ca23
jbisits <jbisits@gmail.com>
2025-12-15 20:19:53 +1100

    Fill more of the t_prev data to avoid finite difference errors

--- src/tracer/MOM_tracer_registry.F90
+++ src/tracer/MOM_tracer_registry.F90
@@ -750,6 +750,11 @@ subroutine post_tracer_diagnostics_at_sync(Reg, h, diag_prev, diag, G, GV, dt)
       enddo ; enddo ; enddo
       call post_data(Tr%id_tendency, work3d, diag, alt_h=diag_prev%h_state)
     endif
+    if (Tr%id_numerical_mixing > 0)
+      do k=1,nz ; do j=js-1,je-1 ; do i=is-1,ie-1
+        tr%t_prev(i,j,k) =  Tr%t(i,j,k)
+      enddo ; enddo ; enddo
+    endif
     if ((Tr%id_trxh_tendency > 0) .or. (Tr%id_trxh_tendency_2d > 0)) then
       do k=1,nz ; do j=js,je ; do i=is,ie
         work3d(i,j,k)     = (Tr%t(i,j,k)*h(i,j,k) - Tr%Trxh_prev(i,j,k)) * Idt
