eec7e0d9180989a23a9982114d1807af10ef3309
jbisits <jbisits@gmail.com>
2025-12-15 20:28:07 +1100

    Forgot then

--- src/tracer/MOM_tracer_registry.F90
+++ src/tracer/MOM_tracer_registry.F90
@@ -750,7 +750,7 @@ subroutine post_tracer_diagnostics_at_sync(Reg, h, diag_prev, diag, G, GV, dt)
       enddo ; enddo ; enddo
       call post_data(Tr%id_tendency, work3d, diag, alt_h=diag_prev%h_state)
     endif
-    if (Tr%id_numerical_mixing > 0)
+    if (Tr%id_numerical_mixing > 0) then
       do k=1,nz ; do j=js-1,je-1 ; do i=is-1,ie-1
         tr%t_prev(i,j,k) =  Tr%t(i,j,k)
       enddo ; enddo ; enddo
